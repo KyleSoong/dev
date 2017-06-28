4fd794afea8e3220e2fe1b47e64eaf08cfa07ad3
KyleSoong <songkh@live.com>
2017-06-28 17:40:21 +0800

    First commit

---
+++ src/main/java/com/song/dev/App.java
@@ -0,0 +1,14 @@
+package com.song.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		
+		System.exit(0);
+	}
+}
