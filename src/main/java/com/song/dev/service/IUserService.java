8b24fa7bf45b3829d4ec8c85a89e973eaa38c5f1
KyleSoong <songkh@live.com>
2017-07-11 17:57:29 +0800

    problem exists

--- src/main/java/com/song/dev/service/IUserService.java
+++ src/main/java/com/song/dev/service/IUserService.java
@@ -3,7 +3,7 @@ package com.song.dev.service;
 import com.song.dev.model.UserAuth;
 import com.song.dev.model.UserInfo;
 
-public interface IUserService {
+public interface IUserService{
     public UserInfo getUserById(Integer userId);
     public String createUser();
     public UserInfo createUser(UserInfo uInfo, UserAuth uAuth);
